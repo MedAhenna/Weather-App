fda9db7c2b77a8985fff9e0eab21de304fc9f9f5
MedAhenna <ahenna.12@gmail.com>
2022-02-20 10:01:29 +0100

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const viewsPath = path.join(__dirname, "../templates/views")
 const partialsPath = path.join(__dirname, "../templates/partials")
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
